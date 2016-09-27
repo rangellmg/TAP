5d28d904885e64a966ce8b848386e4c23329e544
viictormagalhaes <victor.magalhaes@unisul.br>
2016-09-27 20:42:53 -0300

    Exemplar Teste

---
+++ src/TAP/library/Exemplar.java
@@ -0,0 +1,29 @@
+package TAP.library;
+
+public class Exemplar extends Livro {
+	
+	private int codigo;
+	private String localizacao;
+	private String edicao;
+	
+	public int getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+	public String getLocalizacao() {
+		return localizacao;
+	}
+	public void setLocalizacao(String localizacao) {
+		this.localizacao = localizacao;
+	}
+	public String getEdicao() {
+		return edicao;
+	}
+	public void setEdicao(String edicao) {
+		this.edicao = edicao;
+	}
+	
+	
+}
