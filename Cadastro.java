41128dc9f51a45bb87a8a2fb8c8f58ca20d0e889
Rangel Leonardo <rangelllmg@gmail.com>
2016-09-27 20:06:48 -0300

    Add files via upload

---
+++ Cadastro.java
@@ -0,0 +1,36 @@
+package TAP.library;
+
+public class Cadastro {
+
+	private int matricula;
+	private String nome;
+	private String curso;
+	private String locacao;
+	
+	
+	public int getMatricula() {
+		return matricula;
+	}
+	public void setMatricula(int matricula) {
+		this.matricula = matricula;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getCurso() {
+		return curso;
+	}
+	public void setCurso(String curso) {
+		this.curso = curso;
+	}
+	public String getLocacao() {
+		return locacao;
+	}
+	public void setLocacao(String locacao) {
+		this.locacao = locacao;
+	}
+	
+}
---
+++ Livro.java
@@ -0,0 +1,29 @@
+package TAP.library;
+
+public class Livro {
+	
+	private String titulo;
+	private String nome;
+	private String exemplares;
+	
+	public String getTitulo() {
+		return titulo;
+	}
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getExemplares() {
+		return exemplares;
+	}
+	public void setExemplares(String exemplares) {
+		this.exemplares = exemplares;
+	}
+	
+	
+}
