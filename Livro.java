f9c0a76589595136392fb6ee657ab0bf0eb16f20
Rangel Leonardo <rangelllmg@gmail.com>
2016-09-27 20:34:47 -0300

    Add files via upload

--- Livro.java
+++ Livro.java
@@ -2,28 +2,37 @@ package TAP.library;
 
 public class Livro {
 	
+	private final int MAX_EXEMPLAR = 5;
+	private Exemplar exemplares[] = new Exemplar[MAX_EXEMPLAR];
+	
 	private String titulo;
 	private String nome;
-	private String exemplares;
+	private String exemplar;
 	
 	public String getTitulo() {
 		return titulo;
 	}
+	
 	public void setTitulo(String titulo) {
 		this.titulo = titulo;
 	}
+	
 	public String getNome() {
 		return nome;
 	}
+	
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
-	public String getExemplares() {
-		return exemplares;
+
+	public String getExemplar() {
+		return exemplar;
 	}
-	public void setExemplares(String exemplares) {
-		this.exemplares = exemplares;
+
+	public void setExemplar(String exemplar) {
+		this.exemplar = exemplar;
 	}
+	
 	
 	
 }
---
+++ Professor.java
@@ -0,0 +1,15 @@
+package TAP.library;
+
+public class Professor extends Cadastro {
+
+	private String participaCurso;
+
+	public String getParticipaCurso() {
+		return participaCurso;
+	}
+
+	public void setParticipaCurso(String participaCurso) {
+		this.participaCurso = participaCurso;
+	}
+	
+}
